4b3683d9100d1c110192b9355ee895bf34470495
remimoncorge <43540275+remimoncorge@users.noreply.github.com>
2022-06-13 15:27:17 +0200

    clean poster

--- lib/postersession.dart
+++ lib/postersession.dart
@@ -7,9 +7,10 @@ import 'dart:convert';
 class PosterPage extends StatelessWidget {
   final String session;
   final List<Pres> to_show;
-  PosterPage({Key? key, required this.session, required this.to_show})
+  const PosterPage({Key? key, required this.session, required this.to_show})
       : super(key: key);
 
+  // récupère les diférentes thématiques de session poster
   List getPosterSession(to_show) {
     var all_session = [];
     for (var item in to_show) {
@@ -17,42 +18,28 @@ class PosterPage extends StatelessWidget {
         all_session.add(item.session);
       }
     }
-    List<Widget> _sessions = [];
-    for (var theme in all_session) {
-      var new_to_show = to_show
-          .where((Pres) => Pres.jour == 1 && Pres.session == theme)
-          .toList();
-      _sessions.add(Column(
-        children: [
-          Container(child: Text(theme)),
-          Expanded(
-            // The ListView
-            child: ListView.builder(
-              itemCount: new_to_show.length,
-              shrinkWrap: true,
-              itemBuilder: (_, index) {
-                return new_to_show[index];
-              },
-            ),
-          ),
-        ],
-      ));
-    }
     return all_session;
   }
 
   @override
   Widget build(BuildContext context) {
+    // Les widget que je vais charger
     final children = <Widget>[];
 
+    // Je récupère le nom des différentes sessions
     var all_sessions = getPosterSession(to_show);
 
+    // Pour chaque session
     for (var session in all_sessions) {
+      // Je récupère les participants
       List<Pres> new_to_show = to_show
           .where((Pres) => Pres.session == session && Pres.jour == '1')
           .toList();
+      // Texte avec le thème de la session
       children.add(Text('\n' + session + '\n',
           style: TextStyle(fontWeight: FontWeight.bold)));
+
+      // Liste des participants avec leur description
       children.add(
         ListView.builder(
           physics: NeverScrollableScrollPhysics(),
@@ -64,6 +51,7 @@ class PosterPage extends StatelessWidget {
         ),
       );
     }
+
     return Scaffold(
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
