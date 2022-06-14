2825859e41f89331aae8692f4beba6e5ed339804
FIZAINE Florian <fizainef@cg21.priv>
2022-06-14 14:16:22 +0200

    Ajout de l apage d'acceuil en page d'acceuil

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'day1.dart';
+
 class HomePage extends StatelessWidget {
+  final data;
+  HomePage({required this.data});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Page d'acceuil"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.input),
+                title: Text('Home page'),
+                onTap: () => {
+                      Navigator.of(context).pop(),
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomePage(
+                                    data: data,
+                                  )))
+                    }),
+            ListTile(
+              leading: Icon(Icons.verified_user),
+              title: Text('Day 1'),
+              onTap: () => {
+                Navigator.of(context).pop(),
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Day1(data: data, jour: 1)))
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user),
+              title: Text('Day 2'),
+              onTap: () => {
+                Navigator.of(context).pop(),
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Day1(data: data, jour: 2)))
+              },
+            ),
+          ],
+        ),
       ),
       body: Image.asset('assets/Logo.jpeg'),
     );
